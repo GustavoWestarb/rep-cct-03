0ec58e18785934f5e68033073a31d4d2b657402b
bahrP <patrickabahr@gmail.com>
2016-10-11 17:53:46 -0300

    Removendo usuários.

--- src/main/java/com/cedup/dash/repository/Usuarios.java
+++
@@ -1,9 +0,0 @@
-package com.cedup.dash.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cedup.dash.model.Usuario;
-
-public interface Usuarios extends JpaRepository<Usuario, Long>{
-
-}
